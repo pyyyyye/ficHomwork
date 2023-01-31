87f725cf0fa7f5a965d164ac774f40318ed3a096
Park YoungEun <qkzud8033@daum.net>
2023-02-01 05:19:13 +0900

    pye-3 연간 총 생산량 계산
    Signed-off-by: Park YoungEun <qkzud8033@daum.net>

--- components/Farm/index.tsx
+++ components/Farm/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import type { FarmsType } from "libs/type";
+import type { ProductionType, FarmsType } from "libs/type";
+import FarmList from "./FarmList";
 
 const Farm = () => {
   /* TODO: Q2-1 api 통신
@@ -21,7 +22,29 @@ const Farm = () => {
     console.warn("data :", data);
   }, []);
 
-  return <div className="px-2 flex flex-col gap-2"></div>;
+  const total = (production: Array<ProductionType>) => {
+    const Production = production.map((ele) => {
+      return ele.Production;
+    });
+    return Production.reduce((accumulator, curr) => accumulator + curr);
+  };
+
+  return (
+    <div className="px-2 flex flex-col gap-2">
+      {data.map((ele: FarmsType) => {
+        return (
+          <FarmList
+            key={ele.id}
+            id={ele.id}
+            name={ele.name}
+            crops={ele.crops}
+            houses={ele.houses}
+            productionTotal={total(ele.annualProduction)}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Farm;
