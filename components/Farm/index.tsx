cead10831da44bcaad8675f57b17be4b7774b8ea
Park YoungEun <qkzud8033@daum.net>
2023-02-01 05:35:44 +0900

    pye-3 화면조작
    Signed-off-by: Park YoungEun <qkzud8033@daum.net>

--- components/Farm/index.tsx
+++ components/Farm/index.tsx
@@ -4,15 +4,8 @@ import type { ProductionType, FarmsType } from "libs/type";
 import FarmList from "./FarmList";
 
 const Farm = () => {
-  /* TODO: Q2-1 api 통신
-  /* TODO: Q3-1 화면 조작
-  - 호출 받은 데이터중 annualProduction 은 월별 생산량을 가지고 있습니다. 월별 생산량의 총 합을 구하는 함수를 작성해서 
-    계산된 값을 FarmList 컴포넌트의 productionTotal Props 로 내려주세요
-  /* TODO: Q3-2 화면 조작
-  - 랜더링 된 컴포넌트에서 하우스를 동작시키는 함수를 작성해 주세요 역시 예시 이미지를 확인 하세요.
-  */
-
   const [data, setData] = useState<Array<FarmsType>>([]);
+
   useEffect(() => {
     const farmsData = async () => {
       const result = await axios("/api/farm");
@@ -29,6 +22,23 @@ const Farm = () => {
     return Production.reduce((accumulator, curr) => accumulator + curr);
   };
 
+  const onClick = (farmId: number, houseId: number) => {
+    const resData = data.map((ele) => {
+      return ele.id === farmId
+        ? {
+            ...ele,
+            houses: ele.houses?.map((houseData) => {
+              return houseId === houseData.id
+                ? { ...houseData, active: !houseData.active }
+                : { ...houseData };
+            }),
+          }
+        : {
+            ...ele,
+          };
+    });
+    setData(resData);
+  };
   return (
     <div className="px-2 flex flex-col gap-2">
       {data.map((ele: FarmsType) => {
@@ -40,6 +50,7 @@ const Farm = () => {
             crops={ele.crops}
             houses={ele.houses}
             productionTotal={total(ele.annualProduction)}
+            HouseActive={onClick}
           />
         );
       })}
